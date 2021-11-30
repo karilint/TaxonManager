5b006276ee4b7791fe5c7806579f11f91e8475d4
Jonne Sotala <jonne.sotala@gmail.com>
2021-11-30 19:30:13 +0000

    entrypoint script changed

--- app/scripts/entrypoint.prod.sh
+++ app/scripts/entrypoint.prod.sh
@@ -5,4 +5,4 @@ set -e
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn main.wsgi:application --bind 0.0.0.0:8000
+gunicorn taxonmanager.wsgi:application --bind 0.0.0.0:8000

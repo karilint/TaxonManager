93dc421fa39859eb1cdc6b43ea8f219307225ec0
theflyingdolphine <cessicasandra@icloud.com>
2022-01-26 13:52:12 +0200

    styling

--- app/front/urls.py
+++ app/front/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
-    # path('references/', views.view_reference, name='references'),
+    path('references/', views.view_reference, name='references'),
     path('add_reference/', views.add_reference, name='add-references'),
 ]

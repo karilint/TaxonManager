90aa0d1f92b26c5f3a07d9cc386a1da1392cb85a
Tuomas <tietotuomas@gmail.com>
2022-01-29 10:53:44 +0200

    Experimental initial commit

--- app/taxonmanager/settings.py
+++ app/taxonmanager/settings.py
@@ -52,7 +52,16 @@ INSTALLED_APPS = [
     'front.apps.FrontConfig',
 ]
 
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+
 SITE_ID = 1
+
+
 
 SOCIALACCOUNT_PROVIDERS = {
     'orcid': {

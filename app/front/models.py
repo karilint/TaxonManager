c15cffa5ba2beb77f2726bd6d87cd9cf004e6906
Tuomas <tietotuomas@gmail.com>
2022-02-13 19:58:34 +0200

    Revert "Add taxonomicunit"
    This reverts commit 88ec56c3a26ce1a5d11c5ff03aa94c3190d332b2.

--- app/front/models.py
+++ app/front/models.py
@@ -1,11 +1,11 @@
-from django.db import models # pylint:disable=W0611
+from django.db import models  # pylint:disable=W0611
 
 
 # Create your models here.
 class Kingdom(models.Model):
-    """ Defines a kingdom """
     kingdom_name = models.CharField(max_length=100)
-    update_date = models.DateTimeField()
+    update_date = models.DateTimeField('date updated')
+
 
 class TaxonUnitType(models.Model):
     kingdom = models.ForeignKey(Kingdom, on_delete=models.CASCADE)
@@ -13,35 +13,4 @@ class TaxonUnitType(models.Model):
     rank_name = models.CharField(max_length=100)
     dir_parent_rank_id = models.IntegerField(default=0)
     req_parent_rank_id = models.IntegerField(default=0)
-    update_date = models.DateTimeField()
-
-class TaxonomicUnit(models.Model):
-    # TODO: Add tsn if required
-    unit_ind1 = models.CharField(max_length=100)
-    unit_name1 = models.CharField(max_length=100)
-    unit_ind2 = models.CharField(max_length=100)
-    unit_name2 = models.CharField(max_length=100)
-    unit_ind3 = models.CharField(max_length=100)
-    unit_name3 = models.CharField(max_length=100)
-    unit_ind4 = models.CharField(max_length=100)
-    unnamed_taxon_ind = models.CharField(max_length=100)
-    name_usage = models.CharField(max_length=100)
-    unaccept_reason = models.CharField(max_length=100)
-    credibility_rtng = models.CharField(max_length=100)
-    completeness_rtng = models.CharField(max_length=100)
-    currency_rating = models.CharField(max_length=100)
-    phylo_sort_seq = models.IntegerField(default=0)
-    initial_time_stamp = models.DateTimeField()
-    # TODO: Mistä tsn tulee?
-    parent_tsn = models.IntegerField(default=0)
-    taxon_author_id = models.IntegerField(default=0)
-    kingdom = models.ForeignKey(Kingdom, on_delete=models.CASCADE)
-    rank_id = models.IntegerField(default=0)
-    hybrid_author_id = models.IntegerField(default=0)
-    update_date = models.DateTimeField()
-    uncertain_prnt_ind = models.CharField(max_length=100)
-    n_usage = models.CharField(max_length=100)
-    complete_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.id
+    update_date = models.DateTimeField('date updated')

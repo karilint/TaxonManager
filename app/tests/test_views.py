1254dbc6c9e59d37b2ec4c440d602fd8faec9cff
Tuomas <tietotuomas@gmail.com>
2022-02-14 22:48:50 +0200

    Tweak initial tests, add test_user

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,22 +1,43 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
 
 class StatuscodeTest(TestCase):
-    def test_index_page_responses_ok(self):
+
+    def setUp(self):
+        self.test_user = User.objects.create_user(username='Elon Musk')
+
+    def test_index_page_returns_ok(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_references_page_responses_ok(self):
+    def test_references_page_returns_ok(self):
         response = self.client.get('/references/')
         self.assertEqual(response.status_code, 200)
 
-    def test_add_reference_page_responses_redirect(self):
-        response = self.client.get('/add_reference')
-        self.assertEqual(response.status_code, 301)
+    def test_add_reference_page_returns_redirect_before_logging_in(self):
+        response = self.client.get('/add_reference/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_reference_page_returns_ok_after_logging_in(self):
+        self.client.force_login(self.test_user)
+        response = self.client.get('/add_reference/')
+        self.assertEqual(response.status_code, 200)
 
 class TemplateTest(TestCase):
+
+    def setUp(self):
+        self.test_user = User.objects.create_user(username='Elon Musk')
+
     def test_index_page_uses_correct_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'front/index.html')
+
     def test_references_page_uses_correct_template(self):
         response = self.client.get('/references/')
         self.assertTemplateUsed(response, 'front/references.html')
+
+    def test_add_reference_page_uses_correct_template(self):
+        self.client.force_login(self.test_user)
+        response = self.client.get('/add_reference/')
+        self.assertTemplateUsed(response, 'front/add_reference.html')

a685fcdca8d4c650a5e25ac95f8820a680f8b8bb
Tuomas <tietotuomas@gmail.com>
2022-02-15 23:14:49 +0200

    Add tables to Admin panel

--- app/front/admin.py
+++ app/front/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin # pylint:disable=W0611
+from front.models import *
 
-# Register your models here.
+admin.site.register(Comment)
+admin.site.register(Expert)
+admin.site.register(ExpertsGeographicDiv)
+admin.site.register(GeographicDiv)
+admin.site.register(Hierarchy)
+admin.site.register(Kingdom)
+admin.site.register(Publication)
+admin.site.register(ReferenceLink)
+admin.site.register(SynonymLink)
+admin.site.register(TaxonAuthorLkp)
+admin.site.register(TaxonomicUnit)
+admin.site.register(TaxonUnitType)
+admin.site.register(TuCommentLink)

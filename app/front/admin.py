f0c4ce746272dcd9078634a80f4ea579aaf8db2e
Juhana Karhunen <juhana.karhunen@gmail.com>
2022-02-19 11:07:42 +0200

    separate css from base.html, change history visible

--- app/front/admin.py
+++ app/front/admin.py
@@ -6,6 +6,22 @@ from refs.models import Ref
 
 admin.site.unregister(Ref)
 
-class RefAdmin(admin.ModelAdmin):
-    search_fields = ('authors', 'title', 'journal')
-admin.site.register(Ref, SimpleHistoryAdmin)
+class RefAdmin(SimpleHistoryAdmin):
+
+
+    def changed_fields_with_values(self, obj):
+        fields = ""
+        if obj.prev_record:
+            delta = obj.diff_against(obj.prev_record)
+
+            for change in delta.changes:
+                fields += str("{} changed from {} to {}.\n".format(change.field.capitalize(), change.old, change.new))
+            return fields
+        return None
+
+    search_fields = ('title', 'journal')
+    history_list_display = ['changed_fields_with_values']
+
+
+# admin.site.register(Ref, SimpleHistoryAdmin)
+admin.site.register(Ref, RefAdmin)

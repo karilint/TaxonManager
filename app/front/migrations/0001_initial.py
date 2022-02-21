7f3e56a41f11ff18ba3984ecd3e4039ca09ab9ab
Juhnaus <juhana.karhunen@gmail.com>
2022-02-21 22:34:38 +0200

    added migrations for Reference-model

--- app/front/migrations/0001_initial.py
+++ app/front/migrations/0001_initial.py
@@ -13,6 +13,23 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Reference',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('authors', models.CharField(max_length=80)),
+                ('year', models.CharField(max_length=4)),
+                ('edition', models.CharField(max_length=10)),
+                ('volume', models.CharField(max_length=10)),
+                ('no_of_pages', models.CharField(max_length=20)),
+                ('publisher', models.CharField(max_length=20)),
+                ('city', models.CharField(max_length=15)),
+                ('title', models.CharField(max_length=30)),
+                ('journal', models.CharField(max_length=200)),
+                ('bookTitle', models.CharField(max_length=200)),
+                ('referenceType', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Comment',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),

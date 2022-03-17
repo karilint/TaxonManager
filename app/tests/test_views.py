a2d7da685fb8573feac7d16a8ed64870fad5b9f7
Tuomas <tietotuomas@gmail.com>
2022-03-17 22:26:03 +0200

    Add unit tests

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -15,13 +15,22 @@ class StatuscodeTest(TestCase):
         response = self.client.get('/references/')
         self.assertEqual(response.status_code, 200)
 
-    def test_taxons_page_returns_ok(self):
-        response = self.client.get('/taxons/')
+    def test_taxa_page_returns_ok(self):
+        response = self.client.get('/taxa/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_taxa_search_page_returns_ok(self):
+        response = self.client.get('/taxa-search/')
         self.assertEqual(response.status_code, 200)
 
     def test_add_reference_page_returns_ok_after_logging_in(self):
         self.client.force_login(self.test_user)
         response = self.client.get('/add-references/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_addname_page_returns_ok_after_logging_in(self):
+        self.client.force_login(self.test_user)
+        response = self.client.get('/addName/')
         self.assertEqual(response.status_code, 200)
 
 class TemplateTest(TestCase):
@@ -37,12 +46,22 @@ class TemplateTest(TestCase):
         response = self.client.get('/references/')
         self.assertTemplateUsed(response, 'front/references.html')
 
-    def test_taxons_page_uses_correct_template(self):
-        response = self.client.get('/taxons/')
-        self.assertTemplateUsed(response, 'front/taxons.html')
+    def test_taxa_page_uses_correct_template(self):
+        response = self.client.get('/taxa/')
+        self.assertTemplateUsed(response, 'front/taxa.html')
+
+    def test_taxa_search_page_uses_correct_template(self):
+        response = self.client.get('/taxa-search/')
+        self.assertTemplateUsed(response, 'front/taxa-search.html')
 
     def test_add_reference_page_uses_correct_template(self):
         self.client.force_login(self.test_user)
         response = self.client.get('/add-references/')
         self.assertTemplateUsed(response, 'front/add_reference.html')
+
+    def test_addname_page_uses_correct_template(self):
+        self.client.force_login(self.test_user)
+        response = self.client.get('/addName/')
+        self.assertTemplateUsed(response, 'front/add_name.html')
+
 

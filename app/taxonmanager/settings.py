1dfaf2cd1c34b112aa69150523fccf5243f381a7
Tuomas <tietotuomas@gmail.com>
2022-06-27 22:19:27 +0300

    Set PAGE_SIZE:10 as default pagination value for rest_framework API

--- app/taxonmanager/settings.py
+++ app/taxonmanager/settings.py
@@ -223,7 +223,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
 
 ACCOUNT_EMAIL_REQUIRED = True

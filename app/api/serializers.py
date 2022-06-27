16f8b872dc78885bb90c97fdd97c4d0aea5822f0
Tuomas <tietotuomas@gmail.com>
2022-06-27 11:55:04 +0300

    Add ViewSets, DefaultRouter, HyperlinkedModelSerializer

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -6,3 +6,10 @@ class KingdomSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Kingdom
         fields = ['id', 'kingdom_name']
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    kingdom = serializers.HyperlinkedRelatedField(view_name='kingdom-detail', queryset=models.Kingdom.objects.all())
+
+    class Meta:
+        model = models.TaxonAuthorLkp
+        fields = ['taxon_author_id', 'taxon_author', 'kingdom']
--- app/api/views.py
+++ app/api/views.py
@@ -1,11 +1,20 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from front import models
 from api import serializers
 
 
-@api_view(['GET'])
-def getData(request):
-    kingdoms = models.Kingdom.objects.all()
-    serializer = serializers.KingdomSerializer(kingdoms, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def getData(request):
+#     kingdoms = models.Kingdom.objects.all()
+#     serializer = serializers.KingdomSerializer(kingdoms, many=True)
+#     return Response(serializer.data)
+
+class KingdomViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = serializers.KingdomSerializer
+    queryset = models.Kingdom.objects.all()
+
+class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = serializers.AuthorSerializer
+    queryset = models.TaxonAuthorLkp.objects.all()

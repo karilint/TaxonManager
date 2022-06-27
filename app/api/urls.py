6caf82ed94bda8e3f7493fe9be8c0bb9f8c59898
Tuomas <tietotuomas@gmail.com>
2022-06-27 21:36:24 +0300

    Add References, Comments, TaxonUnitTypes, GeographicDivs, Experts, TaxonomicUnits to API & use SlugRelatedField instead of HyperLinked

--- app/api/urls.py
+++ app/api/urls.py
@@ -3,10 +3,20 @@ from rest_framework.routers import DefaultRouter
 from api import views
 
 
-
 router = DefaultRouter()
+router.register('references', views.ReferenceViewSet, basename='reference')
+router.register('comments', views.CommentViewSet, basename='comment')
 router.register('kingdoms', views.KingdomViewSet, basename='kingdom')
-router.register('authors', views.AuthorViewSet, basename='authors_urls')
+router.register('taxonunitypes', views.TaxonUnitTypeViewSet,
+                basename='taxonunittype')
+router.register('geographicdivs', views.GeoGraphicDivViewSet,
+                basename='geographicdiv')
+router.register('authors', views.TaxonAuthorLkpViewSet, basename='author')
+router.register('experts', views.ExpertViewSet, basename='expert')
+
+router.register('taxonomicunits', views.TaxonomicUnitViewSet,
+                basename='taxonomicunit')
+
 
 urlpatterns = [
     path('', include(router.urls))]
--- app/api/views.py
+++ app/api/views.py
@@ -1,20 +1,43 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import viewsets
 from front import models
 from api import serializers
 
 
-# @api_view(['GET'])
-# def getData(request):
-#     kingdoms = models.Kingdom.objects.all()
-#     serializer = serializers.KingdomSerializer(kingdoms, many=True)
-#     return Response(serializer.data)
+class ReferenceViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ReferenceSerializer
+    queryset = models.Reference.objects.all()
 
-class KingdomViewSet(viewsets.ReadOnlyModelViewSet):
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.CommentSerializer
+    queryset = models.Comment.objects.all()
+
+
+class KingdomViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.KingdomSerializer
     queryset = models.Kingdom.objects.all()
 
-class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = serializers.AuthorSerializer
+
+class TaxonUnitTypeViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.TaxonUnitTypeSerializer
+    queryset = models.TaxonUnitType.objects.all()
+
+
+class GeoGraphicDivViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.GeoGraphicDivSerializer
+    queryset = models.GeographicDiv.objects.all()
+
+
+class TaxonAuthorLkpViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.TaxonAuthorLkpSerializer
     queryset = models.TaxonAuthorLkp.objects.all()
+
+
+class ExpertViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ExpertSerializer
+    queryset = models.Expert.objects.all()
+
+
+class TaxonomicUnitViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.TaxonomicUnitSerializer
+    queryset = models.TaxonomicUnit.objects.all()
